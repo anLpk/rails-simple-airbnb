ddeeecf74e6fefe93ff35665b6acbe502406a4d3
anil pak <anilpak35@gmail.com>
2021-01-21 20:22:42 +1100

    Simple Rails Airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Clearing database...'
+Flat.destroy_all
+
+puts 'Creating flats...'
+
+10.times do
+    Flat.create(
+      name: Faker::Address.street_name,
+      address: Faker::Address.street_address,
+      description: Faker::Movies::HarryPotter.quote,
+      price_per_night: rand(100..175),
+      number_of_guests: rand(1..10)
+    )
+end
+
+puts 'Finished!'
